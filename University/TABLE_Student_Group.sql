9d8cbdd0e26a3ad62689f68fd57e33de45fa9db6
River Endless <myowngollum@gmail.com>
2019-09-09 12:49:28 +0300

    Teacher & student group tables' creating & seeding scripts added

---
+++ University/TABLE_Student_Group.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Student_group
+(
+    CodeNumber VARCHAR(3) PRIMARY KEY,
+    Name VARCHAR(4),
+    CountOfMembers INT CHECK(CountOfMembers > 0),
+    Specialty VARCHAR(100),
+	LeaderLastName VARCHAR(20)
+)
+
+INSERT INTO Student_group 
+(CodeNumber, Name, CountOfMembers, Specialty, LeaderLastName) 
+VALUES
+('8Г', 'Э-12', 18, 'ЭВМ', 'Иванова'),
+('7Г', 'Э-15', 22, 'ЭВМ', 'Сеткин'),
+('4Г', 'АС-9', 24, 'АСОИ', 'Балабанов'),
+('3Г', 'АС-8', 20, 'АСОИ', 'Чижов'),
+('17Г', 'С-14', 29, 'СД', 'Амросов'),
+('12Г', 'М-6', 16, 'Международная экономика', 'Трубин'),
+('10Г', 'Б-4 ', 21, 'Бухучета', 'Зязюткин')
---
+++ University/TABLE_Teacher.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Teacher
+(
+    PersonalNumber VARCHAR(3) PRIMARY KEY,
+    LastName VARCHAR(20),
+    Position VARCHAR(20),
+    Department VARCHAR(50),
+    Specialty VARCHAR(1000),
+	HomePhone VARCHAR(3)
+)
+
+INSERT INTO Teacher 
+(PersonalNumber, LastName, Position, Department, Specialty, HomePhone) 
+VALUES
+('221Л', 'Фролов', 'Доцент', 'ЭВМ', 'АСОИ, ЭВМ', '487'),
+('222Л', 'Костин', 'Доцент', 'ЭВМ', 'ЭВМ', '543'),
+('225Л', 'Бойко', 'Профессор', 'АСУ', 'АСОИ, ЭВМ', '112'),
+('430Л', 'Глазов', 'Ассистент', 'ТФ', 'СД', '421'),
+('110Л', 'Петров', 'Ассистент', 'Экономики', 'Международная экономика', '324')
